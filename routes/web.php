1db4984bccf2f1da8dccaa46798a533dc6c74b12
keshawa <keshawa@technium.com.au>
2024-05-13 19:58:07 +1000

    introduce layouts and include navigation

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [DashboardController::class, 'index']);
+
+Route::get('/profile', [ProfileController::class, 'index']);
+
+Route::get('/terms', function () {
+    return view('terms');
 });
