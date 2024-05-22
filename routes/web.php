e1940f3397632440898d6373a5a36077cc341856
keshawa <keshawa@technium.com.au>
2024-05-22 21:34:36 +1000

    form submission step 1

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,15 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\IdeaController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [DashboardController::class, 'index']);
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
 Route::get('/profile', [ProfileController::class, 'index']);
+
+Route::post('/idea', [IdeaController::class, 'store'])->name('idea.create');
 
 Route::get('/terms', function () {
     return view('terms');
