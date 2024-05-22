013a5ae4db88bb753879e4b5564e7b39ea7c0661
keshawa <keshawa@technium.com.au>
2024-05-22 22:39:37 +1000

    pagination with boostrap

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -42,9 +42,13 @@
         <hr>
         @foreach ($ideas as $idea)
             <div class="mt-3">
-                @include('shared.idea_card');
+                @include('shared.idea_card')
             </div>
         @endforeach
+        <div class ="mt-3">
+            {{ $ideas->links() }}
+        </div>
+      
     </div>
     <div class="col-3">
         <div class="card">
