8b87f9735b36c8f2ba3565f75a463d20d2e90010
keshawa <keshawa@technium.com.au>
2024-08-30 15:09:14 +1000

    many to many relationships with pivot table, follow unfollow users

--- app/Http/Controllers/FollowerController.php
+++ app/Http/Controllers/FollowerController.php
@@ -7,6 +7,12 @@ use App\Models\User;
 
 class FollowerController extends Controller
 {
+    /**
+     * Follow a user.
+     *
+     * @param \App\Models\User $user The user to follow.
+     * @return \Illuminate\Http\RedirectResponse Redirects to the user's profile page with a success message.
+     */
     public function follow(User $user)
     {
         $follower = auth()->user();
@@ -14,6 +20,12 @@ class FollowerController extends Controller
         return redirect()->route('users.show', $user->id)->with('success', 'You are now following this user.');
     }
 
+    /**
+     * Unfollow a user.
+     *
+     * @param \App\Models\User $user The user to unfollow.
+     * @return \Illuminate\Http\RedirectResponse Redirects to the user's profile page with a success message.
+     */
     public function unfollow($user)
     {
         $follower = auth()->user();
